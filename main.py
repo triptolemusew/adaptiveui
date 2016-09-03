a1132a57f5d53566edc9b7cd3ac2c7b90a2ddfcb
triptolemusew <deneuve.crepes@gmail.com>
2016-09-03 11:59:14 +0800

    Something

--- main.py
+++ main.py
@@ -8,6 +8,8 @@
 from PyQt4 import QtCore, QtGui
 from PyQt4 import phonon
 from PyQt4.phonon import Phonon
+import cv2
+import sys
 import glob, os
 
 try:
@@ -608,10 +610,39 @@ class Ui_MainWindow(QtGui.QMainWindow):
             self.music_table.setRowHidden(i, False)
 
     def cameraStack(self):
-        layout = QtGui.QFormLayout()
-        layout.addRow("Name", QtGui.QLineEdit())
-        layout.addRow("Address", QtGui.QLineEdit())
-        self.page_4.setLayout(layout)
+        # layout = QtGui.QFormLayout()
+        # layout.addRow("Name", QtGui.QLineEdit())
+        # layout.addRow("Address", QtGui.QLineEdit())
+        # self.page_4.setLayout(layout)
+
+        # cascPath = sys.argv[1]
+        faceCascade = cv2.CascadeClassifier('asd.xml')
+
+        video_capture = cv2.VideoCapture(0)
+
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+        )
+
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # Display the resulting frame
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            sys.exit()
+
 
     def displayStack(self):
         if self.stackedWidget.currentIndex() == 1:
